ac00aa78ff1ef54bc78d3d4ed44f322c23c35cc7
Bhavya Jain <bhavya16081997@gmail.com>
2018-11-30 20:44:12 +0530

    Update findFuncDefn.cpp

--- findFuncDefn.cpp
+++ findFuncDefn.cpp
@@ -9,37 +9,37 @@ using namespace std;
 #define MAX 255
 
 unsigned int FindFunctionDefn( const char* strFunctionName, const char* strSourceCode ) {
-    FILE * fptr;                                                        //fptr to point to the file
+    FILE * fptr;                                                       		//fptr to point to the file
 
-    fptr = fopen(strSourceCode, "r");                                        //reads the given file
+    fptr = fopen(strSourceCode, "r");                                        	//reads the given file
 
-    char buffer[MAX];                                                   //stores a line while reading the file
-    int line = 0, length = strlen(strFunctionName);                            //line: retains a count of the line ; length: stores the length of the function name
-    vector<char> s;                                                     //utility stack
+    char buffer[MAX];                                                   	//stores a line while reading the file
+    int line = 0, length = strlen(strFunctionName);                             //line: retains a count of the line ; length: stores the length of the function name
+    vector<char> s;                                                     	//utility stack
 
-    while(fgets(buffer, 255, fptr) != NULL) {                           //reads the file line by line
+    while(fgets(buffer, 255, fptr) != NULL) {                           	//reads the file line by line
       line++;
 
       string buff(buffer);
-      int found = buff.find(strFunctionName);                                  //stores the first occurence of the function name in the line
+      int found = buff.find(strFunctionName);                                   //stores the first occurence of the function name in the line
 
-  		if(found != string::npos) {                                       //if successfully found
+  		if(found != string::npos) {                                     //if successfully found
         int index;
 
-        if(buff[found + length] == ' ') {                               //token ends here
+        if(buff[found + length] == ' ') {                               	//token ends here
   			     index = found + length + 1;
         } else {
   			     index = found + length;
   		  }
 
         while(buff[index] != '\0') {
-          if(buff[index] == '(') {                                    //marks the start of the parameters to be passed, if any
+          if(buff[index] == '(') {                                    		//marks the start of the parameters to be passed, if any
             s.push_back('(');
-          } else if(buff[index] == ')' && !s.empty()) {               //marks the end of the parameters' list, if any
+          } else if(buff[index] == ')' && !s.empty()) {               		//marks the end of the parameters' list, if any
             s.pop_back();
-          } else if(buff[index] == '{' && s.empty()) {                //marks the start of the function definition
+          } else if(buff[index] == '{' && s.empty()) {                		//marks the start of the function definition
             return line;
-          } else if(buff[index] == ';') {                             //marks that function wasn't defined but might have been called
+          } else if(buff[index] == ';') {                             		//marks that function wasn't defined but might have been called
             s.clear();
             break;
           }
@@ -53,12 +53,12 @@ unsigned int FindFunctionDefn( const char* strFunctionName, const char* strSourc
 }
 
 int main() {
-    char strSourceCode[20], strFunctionName[20];                         //strSourceCode: stores the code-file ; strFunctionName: stores the function name to be searched
+    char strSourceCode[20], strFunctionName[20];                         	//strSourceCode: stores the code-file ; strFunctionName: stores the function name to be searched
 
     cout << "Enter an existing filename and function name to be found: ";
     cin >> strSourceCode >> strFunctionName;
 
-    while(1) {                                                          //checks for existing filename
+    while(1) {                                                          	//checks for existing filename
       if(access(strSourceCode, F_OK) == 0)
 		    break;
       else {
